799877e073da4b9682204d9e5e6d5845e0a31a9d
Alex Naupay <alexnaupay@gmail.com>
2024-06-01 00:44:39 -0500

    /users post

---
+++ users/create.js
@@ -0,0 +1,46 @@
+const AWS = require('aws-sdk')
+const { randomUUID } = require("crypto")
+
+let dynamoDBClientParams = {}
+
+if (process.env.IS_OFFLINE) {
+    dynamoDBClientParams =  {
+        region: 'localhost',
+        endpoint: 'http://localhost:8000',
+        accessKeyId: 'MockAccessKey',  // needed if you don't have aws credentials at all in env
+        secretAccessKey: 'MockSecretKey' // needed if you don't have aws credentials at all in env
+    }
+}
+
+const dynamodb = new AWS.DynamoDB.DocumentClient(dynamoDBClientParams)
+
+const store = async (event, context) => {
+    // console.info(event.body, typeof event.body)
+
+    const body = JSON.parse(event.body)
+
+    const uuid = randomUUID()
+
+    const user =  {name: body.name}
+    user.pk = uuid
+
+    let params = {
+        TableName: 'usersTable',
+        Item: user
+    };
+
+    return dynamodb.put(params).promise()
+        .then((response) => {
+            console.info(response);
+            return {
+                statusCode: 201,
+                body: JSON.stringify(user)
+            }
+        })
+};
+
+
+
+module.exports = {
+    store
+}
